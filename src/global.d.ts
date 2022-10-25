ee643f38a075ad22f140b4861825e612e83f28f6
Prajwal264 <prajwalp264@gmail.com>
2022-10-25 14:53:19 +0530

    guess i need to custom implement this dnd

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="svelte" />
-declare type Item = import('svelte-dnd-action').Item;
-declare type DndEvent<ItemType = Item> = import('svelte-dnd-action').DndEvent<ItemType>;
-declare namespace svelte.JSX {
-  interface HTMLAttributes<T> {
-    onconsider?: (event: CustomEvent<DndEvent<ItemType>> & { target: EventTarget & T }) => void;
-    onfinalize?: (event: CustomEvent<DndEvent<ItemType>> & { target: EventTarget & T }) => void;
-  }
-}
